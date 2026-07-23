8876e3f239227d1a3eace91ce44d1846f293f393
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-07-23 21:26:18 +0000

    license: mark rewritten interfaces as Shitty-owned

--- vterm.h
+++ vterm.h
@@ -4,21 +4,11 @@
  * See the file LICENSE.MIT for the full license.
  */
 
-/* part of this file is part of Zutty.
- * Copyright (C) 2020 Tom Szilagyi
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * See the file LICENSE.GPL3 for the full license.
- */
-
 #pragma once
-#include <std/sys/types.h>
 
 #include "terminal_types.h"
+
+#include <std/sys/types.h>
 
 #include <cstddef>
 #include <cstdint>
