f9c126291649a5b7104ea63d1b7e3864e89851b5
Anton Samokhvalov <anton@samokhvalov.xyz>
2026-07-25 22:22:34 +0000

    style: format color stream specialization

--- color.cpp
+++ color.cpp
@@ -14,10 +14,8 @@
 using namespace stl;
 
 namespace stl {
-
     template <>
     void output<ZeroCopyOutput, ::Color>(ZeroCopyOutput& output, ::Color color) {
         output << StringView(u8"rgb:") << Hex{(u64)(color.red) * 0x101, 4} << StringView(u8"/") << Hex{(u64)(color.green) * 0x101, 4} << StringView(u8"/") << Hex{(u64)(color.blue) * 0x101, 4};
     }
-
 }
