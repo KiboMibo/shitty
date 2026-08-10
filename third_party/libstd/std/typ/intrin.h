f2a4612e33bd25f73b0639a86b15f319d8e1aac8
Anton Samokhvalov <pg@yandex-team.ru>
2026-08-10 05:46:54 +0300

    libstd: support trivial destruction on GCC 15

--- third_party/libstd/std/typ/intrin.h
+++ third_party/libstd/std/typ/intrin.h
@@ -3,6 +3,10 @@
 // portable defines for compiler intrinsics
 
 #define stdIsTriviallyCopyable __is_trivially_copyable
-#define stdHasTrivialDestructor __is_trivially_destructible
+#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ < 16
+    #define stdHasTrivialDestructor __has_trivial_destructor
+#else
+    #define stdHasTrivialDestructor __is_trivially_destructible
+#endif
 #define stdIsTrivial __is_trivial
 #define stdIsStandardLayout __is_standard_layout
